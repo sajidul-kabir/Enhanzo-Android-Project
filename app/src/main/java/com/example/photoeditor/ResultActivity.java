fcba47551f14dd18e9b590ff06780d2f1ccbfc22
AnindoSF <sarkeranindo@gmail.com>
2021-07-26 21:40:48 +0600

    Share function and redesign

--- app/src/main/java/com/example/photoeditor/ResultActivity.java
+++ app/src/main/java/com/example/photoeditor/ResultActivity.java
@@ -2,7 +2,9 @@ package com.example.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.photoeditor.databinding.ActivityResultBinding;
 
@@ -18,5 +20,25 @@ public class ResultActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         binding.image.setImageURI(getIntent().getData());
+
+
+        binding.shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String body = "Check out this app !!!";
+                String sub = "Testing Funcitionality";
+                intent.putExtra(Intent.EXTRA_TEXT,body);
+                intent.putExtra(Intent.EXTRA_TEXT,sub);
+                startActivity(Intent.createChooser(intent,"Share using"));
+
+            }
+        });
+
+
+
+
+
     }
 }
