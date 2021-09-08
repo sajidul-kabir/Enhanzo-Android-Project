4f4f745792871b6e23be51ea2cd66e857a6f5c82
Anindo Sarker <sarkeranindo@gmail.com>
2021-09-09 01:56:20 +0600

    Share function works

--- app/src/main/java/com/example/imagefilters/activities/filteredimage/FilteredImageActivity.kt
+++ app/src/main/java/com/example/imagefilters/activities/filteredimage/FilteredImageActivity.kt
@@ -1,12 +1,42 @@
 package com.example.imagefilters.activities.filteredimage
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.imagefilters.R
+import com.example.imagefilters.activities.editimage.EditImageActivity
+import com.example.imagefilters.databinding.ActivityFilteredImageBinding
 
 class FilteredImageActivity : AppCompatActivity() {
+
+
+    private lateinit var fileUri: Uri
+    private lateinit var binding: ActivityFilteredImageBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_filtered_image)
+        binding = ActivityFilteredImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        displayFilteredImage()
+        setListeners()
+    }
+
+    private fun displayFilteredImage() {
+        intent.getParcelableExtra<Uri>(EditImageActivity.KEY_FILTERED_IMAGE_URI)?.let { imageUri ->
+            fileUri = imageUri
+            binding.imageFilteredImage.setImageURI(imageUri)
+        }
+    }
+
+    private fun setListeners() {
+        binding.fabShare.setOnClickListener {
+            with(Intent(Intent.ACTION_SEND)) {
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                type = "image/*"
+                startActivity(this)
+            }
+        }
     }
 }
