3780f86642cbad12b719d86183d053fe5a992ea2
Sajidul Kabir <sajidulkabiradib@gmail.com>
2021-09-15 18:47:16 +0600

    camera functionality add

--- app/src/main/java/com/example/enhanzo/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/enhanzo/activities/main/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.enhanzo.activities.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.enhanzo.activities.editimage.EditImageActivity
 import com.example.enhanzo.activities.savedimages.SavedImagesActivity
 import com.example.enhanzo.databinding.ActivityMainBinding
-
+import java.io.ByteArrayOutputStream
+import java.io.File
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -16,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
+    private var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +46,26 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+        binding.camera.setOnClickListener {
+
+            val takePictureIntent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+
+            if(takePictureIntent.resolveActivity(this.packageManager)!=null){
+                startActivityForResult(takePictureIntent,32)
+            }
+            else{
+                Toast.makeText(this,"unable to open camera",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName,".jpg",storageDirectory)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -52,6 +78,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        else if(requestCode == 32 && resultCode == RESULT_OK){
+            //    val takenImage = data?.extras?.get("data") as Bitmap
+            val takenImage = data?.extras?.get("data") as Bitmap
+            val uri = getImageUri(takenImage)
+
+
+            //Toast.makeText(this,"uri is"+uri,Toast.LENGTH_SHORT).show()
+
+            Intent(applicationContext,EditImageActivity::class.java).also { cameraIntent->
+                cameraIntent.putExtra(KEY_IMAGE_URI,uri)
+                startActivity(cameraIntent)
+            }
+
+        }
+
     }
 
+
+    fun getImageUri(bitmap:Bitmap): Uri? {
+        val arrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, arrayOutputStream)
+        val path = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "Title", null)
+        return Uri.parse(path)
+
+    }
 }
